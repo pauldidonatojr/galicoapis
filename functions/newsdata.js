a8e27fdec4602c3e8e62999ad69f1c0e1e2d38a0
PJ <87795990+pauldidonatojr@users.noreply.github.com>
2023-03-14 12:36:22 -0400

    push me

--- functions/newsdata.js
+++ functions/newsdata.js
@@ -23,7 +23,7 @@ exports.handler = async (event, context, callback) => {
 
     callback(null, {
       headers: {
-        'Access-Control-Allow-Origin': 'https://galiconewsapi.netlify.app/api/newsdata',
+        'Access-Control-Allow-Origin': 'http://localhost:3000',
         'Access-Control-Allow-Headers': 'Content-Type',
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
       },
@@ -34,7 +34,7 @@ exports.handler = async (event, context, callback) => {
     console.error(error);
     callback(error, {
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': 'https://galiconewsapi.netlify.app/api/newsdata',
         'Access-Control-Allow-Headers': 'Content-Type',
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
       },
